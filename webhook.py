25fcab45981f0da3d5ae5e124848e83550a2508f
Declan Zevan <declan.zevan@gmail.com>
2025-02-15 12:55:51 -0500

    Fixed an error in the `send` function definition/declaration

--- webhook.py
+++ webhook.py
@@ -9,7 +9,7 @@ class DiscordWebhookSender:
     def __init__(self, url):
         self.url = url
 
-    async def send(self, content: str = MISSING, *, username: str = MISSING, avatar_url: Any = MISSING, tts: bool = False, ephemeral: bool = False, file: discord.File = MISSING, files: Sequence[discord.File] = MISSING, embed: discord.Embed = MISSING, embeds: Sequence[discord.Embed] = MISSING, allowed_mentions: discord.AllowedMentions = MISSING, view: discord.View = MISSING, thread: discord.Snowflake = MISSING, thread_name: str = MISSING, wait: bool = False, suppress_embeds: bool = False, silent: bool = False, applied_tags: List[discord.ForumTag] = MISSING, poll: discord.Poll = MISSING):
+    async def send(self, content: str = MISSING, *, username: str = MISSING, avatar_url: Any = MISSING, tts: bool = False, ephemeral: bool = False, file: discord.File = MISSING, files: Sequence[discord.File] = MISSING, embed: discord.Embed = MISSING, embeds: Sequence[discord.Embed] = MISSING, allowed_mentions: discord.AllowedMentions = MISSING, view: discord.ui.View = MISSING, thread: discord.Snowflake = MISSING, thread_name: str = MISSING, wait: bool = False, suppress_embeds: bool = False, silent: bool = False, applied_tags: List[discord.ForumTag] = MISSING, poll: discord.Poll = MISSING):
         async with aiohttp.ClientSession() as session:
             webhook = Webhook.from_url(self.url, session=session)
             await webhook.send(content=content, username=username, avatar_url=avatar_url, tts=tts, ephemeral=ephemeral, file=file, files=files, embed=embed, embeds=embeds, allowed_mentions=allowed_mentions, view=view, thread=thread, thread_name=thread_name, wait=wait, suppress_embeds=suppress_embeds, silent=silent, applied_tags=applied_tags, poll=poll)
