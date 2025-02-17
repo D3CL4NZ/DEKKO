73f5bcdc77c2c92521493ce3b73d51266f107f81
Declan Zevan <declan.zevan@gmail.com>
2025-02-16 21:34:56 -0500

    Small patch to fix holiday channels

--- extensions/holidays.py
+++ extensions/holidays.py
@@ -65,89 +65,95 @@ class Holidays(commands.Cog):
     async def _check_if_holiday(self):
         await self.bot.wait_until_ready()
 
-        NEW_YEARS_CHANNELS = await db.fetch("SELECT new_years_channel FROM holidata")
-        CN_NEW_YEARS_CHANNELS = await db.fetch("SELECT cn_new_years_channel FROM holidata")
-        VDAY_CHANNELS = await db.fetch("SELECT vday_channel FROM holidata")
-        ST_PATTY_CHANNELS = await db.fetch("SELECT st_patricks_channel FROM holidata")
-        EASTER_CHANNELS = await db.fetch("SELECT easter_channel FROM holidata")
-        CDM_CHANNELS = await db.fetch("SELECT cinco_de_mayo_channel FROM holidata")
-        J4_CHANNELS = await db.fetch("SELECT j4_channel FROM holidata")
-        HALLOWEEN_CHANNELS = await db.fetch("SELECT halloween_channel FROM holidata")
-        THANKSGIVING_CHANNELS = await db.fetch("SELECT thanksgiving_channel FROM holidata")
-        CHRISTMAS_CHANNELS = await db.fetch("SELECT christmas_channel FROM holidata")
-        WISHLIST_CHANNELS = await db.fetch("SELECT wishlist_channel FROM holidata")
+        for guild in self.bot.guilds:
+            NEW_YEARS_CHANNEL_ID = await db.fetch("SELECT new_years_channel FROM holidata WHERE guild = ?", guild.id)
+            CN_NEW_YEARS_CHANNEL_ID = await db.fetch("SELECT cn_new_years_channel FROM holidata WHERE guild = ?", guild.id)
+            VDAY_CHANNEL_ID = await db.fetch("SELECT vday_channel FROM holidata WHERE guild = ?", guild.id)
+            ST_PATTY_CHANNEL_ID = await db.fetch("SELECT st_patricks_channel FROM holidata WHERE guild = ?", guild.id)
+            EASTER_CHANNEL_ID = await db.fetch("SELECT easter_channel FROM holidata WHERE guild = ?", guild.id)
+            CDM_CHANNEL_ID = await db.fetch("SELECT cinco_de_mayo_channel FROM holidata WHERE guild = ?", guild.id)
+            J4_CHANNEL_ID = await db.fetch("SELECT j4_channel FROM holidata WHERE guild = ?", guild.id)
+            HALLOWEEN_CHANNEL_ID = await db.fetch("SELECT halloween_channel FROM holidata WHERE guild = ?", guild.id)
+            THANKSGIVING_CHANNEL_ID = await db.fetch("SELECT thanksgiving_channel FROM holidata WHERE guild = ?", guild.id)
+            CHRISTMAS_CHANNEL_ID = await db.fetch("SELECT christmas_channel FROM holidata WHERE guild = ?", guild.id)
+            WISHLIST_CHANNEL_ID = await db.fetch("SELECT wishlist_channel FROM holidata WHERE guild = ?", guild.id)
 
-        for NEW_YEARS_CHANNEL, CN_NEW_YEARS_CHANNEL, VDAY_CHANNEL, ST_PATTY_CHANNEL, EASTER_CHANNEL, CDM_CHANNEL, J4_CHANNEL, HALLOWEEN_CHANNEL, THANKSGIVING_CHANNEL, CHRISTMAS_CHANNEL, WISHLIST_CHANNEL in zip(NEW_YEARS_CHANNELS, CN_NEW_YEARS_CHANNELS, VDAY_CHANNELS, ST_PATTY_CHANNELS, EASTER_CHANNELS, CDM_CHANNELS, J4_CHANNELS, HALLOWEEN_CHANNELS, THANKSGIVING_CHANNELS, CHRISTMAS_CHANNELS, WISHLIST_CHANNELS):
-            NEW_YEARS_CHANNEL = self.bot.get_channel(NEW_YEARS_CHANNEL)
-            CN_NEW_YEARS_CHANNEL = self.bot.get_channel(CN_NEW_YEARS_CHANNEL)
-            VDAY_CHANNEL = self.bot.get_channel(VDAY_CHANNEL)
-            ST_PATTY_CHANNEL = self.bot.get_channel(ST_PATTY_CHANNEL)
-            EASTER_CHANNEL = self.bot.get_channel(EASTER_CHANNEL)
-            CDM_CHANNEL = self.bot.get_channel(CDM_CHANNEL)
-            J4_CHANNEL = self.bot.get_channel(J4_CHANNEL)
-            HALLOWEEN_CHANNEL = self.bot.get_channel(HALLOWEEN_CHANNEL)
-            THANKSGIVING_CHANNEL = self.bot.get_channel(THANKSGIVING_CHANNEL)
-            CHRISTMAS_CHANNEL = self.bot.get_channel(CHRISTMAS_CHANNEL)
-            WISHLIST_CHANNEL = self.bot.get_channel(WISHLIST_CHANNEL)
+            NEW_YEARS_CHANNEL = guild.get_channel(NEW_YEARS_CHANNEL_ID[0]) if not(NEW_YEARS_CHANNEL_ID is None or (isinstance(NEW_YEARS_CHANNEL_ID, tuple) and all(url is None for url in NEW_YEARS_CHANNEL_ID))) else None
+            CN_NEW_YEARS_CHANNEL = guild.get_channel(CN_NEW_YEARS_CHANNEL_ID[0]) if not(CN_NEW_YEARS_CHANNEL_ID is None or (isinstance(CN_NEW_YEARS_CHANNEL_ID, tuple) and all(url is None for url in CN_NEW_YEARS_CHANNEL_ID))) else None
+            VDAY_CHANNEL = guild.get_channel(VDAY_CHANNEL_ID[0]) if not(VDAY_CHANNEL_ID is None or (isinstance(VDAY_CHANNEL_ID, tuple) and all(url is None for url in VDAY_CHANNEL_ID))) else None
+            ST_PATTY_CHANNEL = guild.get_channel(ST_PATTY_CHANNEL_ID[0]) if not(ST_PATTY_CHANNEL_ID is None or (isinstance(ST_PATTY_CHANNEL_ID, tuple) and all(url is None for url in ST_PATTY_CHANNEL_ID))) else None
+            EASTER_CHANNEL = guild.get_channel(EASTER_CHANNEL_ID[0]) if not(EASTER_CHANNEL_ID is None or (isinstance(EASTER_CHANNEL_ID, tuple) and all(url is None for url in EASTER_CHANNEL_ID))) else None
+            CDM_CHANNEL = guild.get_channel(CDM_CHANNEL_ID[0]) if not(CDM_CHANNEL_ID is None or (isinstance(CDM_CHANNEL_ID, tuple) and all(url is None for url in CDM_CHANNEL_ID))) else None
+            J4_CHANNEL = guild.get_channel(J4_CHANNEL_ID[0]) if not(J4_CHANNEL_ID is None or (isinstance(J4_CHANNEL_ID, tuple) and all(url is None for url in J4_CHANNEL_ID))) else None
+            HALLOWEEN_CHANNEL = guild.get_channel(HALLOWEEN_CHANNEL_ID[0]) if not(HALLOWEEN_CHANNEL_ID is None or (isinstance(HALLOWEEN_CHANNEL_ID, tuple) and all(url is None for url in HALLOWEEN_CHANNEL_ID))) else None
+            THANKSGIVING_CHANNEL = guild.get_channel(THANKSGIVING_CHANNEL_ID[0]) if not(THANKSGIVING_CHANNEL_ID is None or (isinstance(THANKSGIVING_CHANNEL_ID, tuple) and all(url is None for url in THANKSGIVING_CHANNEL_ID))) else None
+            CHRISTMAS_CHANNEL = guild.get_channel(CHRISTMAS_CHANNEL_ID[0]) if not(CHRISTMAS_CHANNEL_ID is None or (isinstance(CHRISTMAS_CHANNEL_ID, tuple) and all(url is None for url in CHRISTMAS_CHANNEL_ID))) else None
+            WISHLIST_CHANNEL = guild.get_channel(WISHLIST_CHANNEL_ID[0]) if not(WISHLIST_CHANNEL_ID is None or (isinstance(WISHLIST_CHANNEL_ID, tuple) and all(url is None for url in WISHLIST_CHANNEL_ID))) else None
 
-            if NEW_YEARS_CHANNEL is None or CN_NEW_YEARS_CHANNEL is None or VDAY_CHANNEL is None or ST_PATTY_CHANNEL is None or EASTER_CHANNEL is None or CDM_CHANNEL is None or J4_CHANNEL is None or HALLOWEEN_CHANNEL is None or THANKSGIVING_CHANNEL is None or CHRISTMAS_CHANNEL is None or WISHLIST_CHANNEL is None:
-                common.logger.warning("[Holidays] One or more holiday channels are missing. Please run `/setup holidays`.")
-                return
+            if NEW_YEARS_CHANNEL:
+                if (date(date.today().year, 12, 25) - timedelta(days=1) <= date.today() <= date(date.today().year, 12, 31) + timedelta(days=1)):
+                    await NEW_YEARS_CHANNEL.set_permissions(NEW_YEARS_CHANNEL.guild.default_role, view_channel=None)
+                elif (date(date.today().year, 1, 1) - timedelta(days=1) <= date.today() <= date(date.today().year, 1, 1) + timedelta(days=1)):
+                    await NEW_YEARS_CHANNEL.set_permissions(NEW_YEARS_CHANNEL.guild.default_role, view_channel=None)
+                else:
+                    await NEW_YEARS_CHANNEL.set_permissions(NEW_YEARS_CHANNEL.guild.default_role, view_channel=False)
 
-            if (date(date.today().year, 12, 25) - timedelta(days=1) <= date.today() <= date(date.today().year, 12, 31) + timedelta(days=1)):
-                await NEW_YEARS_CHANNEL.set_permissions(NEW_YEARS_CHANNEL.guild.default_role, view_channel=None)
-            elif (date(date.today().year, 1, 1) - timedelta(days=1) <= date.today() <= date(date.today().year, 1, 1) + timedelta(days=1)):
-                await NEW_YEARS_CHANNEL.set_permissions(NEW_YEARS_CHANNEL.guild.default_role, view_channel=None)
-            else:
-                await NEW_YEARS_CHANNEL.set_permissions(NEW_YEARS_CHANNEL.guild.default_role, view_channel=False)
+            if CN_NEW_YEARS_CHANNEL:
+                # Due to the extreme complexities in determining the date for Chinese New Year, I've decided to just use a library for it
+                if (get_cny_date(date.today().year) - timedelta(days=1) <= date.today() <= get_cny_date(date.today().year) + timedelta(days=1)):
+                    await CN_NEW_YEARS_CHANNEL.set_permissions(CN_NEW_YEARS_CHANNEL.guild.default_role, view_channel=None)
+                else:
+                    await CN_NEW_YEARS_CHANNEL.set_permissions(CN_NEW_YEARS_CHANNEL.guild.default_role, view_channel=False)
 
-            # Due to the extreme complexities in determining the date for Chinese New Year, I've decided to just use a library for it
-            if (get_cny_date(date.today().year) - timedelta(days=1) <= date.today() <= get_cny_date(date.today().year) + timedelta(days=1)):
-                await CN_NEW_YEARS_CHANNEL.set_permissions(CN_NEW_YEARS_CHANNEL.guild.default_role, view_channel=None)
-            else:
-                await CN_NEW_YEARS_CHANNEL.set_permissions(CN_NEW_YEARS_CHANNEL.guild.default_role, view_channel=False)
+            if VDAY_CHANNEL:
+                if (date(date.today().year, 2, 1) - timedelta(days=1) <= date.today() <= date(date.today().year, 2, 14) + timedelta(days=1)):
+                    await VDAY_CHANNEL.set_permissions(VDAY_CHANNEL.guild.default_role, view_channel=None)
+                else:
+                    await VDAY_CHANNEL.set_permissions(VDAY_CHANNEL.guild.default_role, view_channel=False)
 
-            if (date(date.today().year, 2, 1) - timedelta(days=1) <= date.today() <= date(date.today().year, 2, 14) + timedelta(days=1)):
-                await VDAY_CHANNEL.set_permissions(VDAY_CHANNEL.guild.default_role, view_channel=None)
-            else:
-                await VDAY_CHANNEL.set_permissions(VDAY_CHANNEL.guild.default_role, view_channel=False)
+            if ST_PATTY_CHANNEL:
+                if (date(date.today().year, 3, 17) - timedelta(days=1) <= date.today() <= date(date.today().year, 3, 17) + timedelta(days=1)):
+                    await ST_PATTY_CHANNEL.set_permissions(ST_PATTY_CHANNEL.guild.default_role, view_channel=None)
+                else:
+                    await ST_PATTY_CHANNEL.set_permissions(ST_PATTY_CHANNEL.guild.default_role, view_channel=False)
 
-            if (date(date.today().year, 3, 17) - timedelta(days=1) <= date.today() <= date(date.today().year, 3, 17) + timedelta(days=1)):
-                await ST_PATTY_CHANNEL.set_permissions(ST_PATTY_CHANNEL.guild.default_role, view_channel=None)
-            else:
-                await ST_PATTY_CHANNEL.set_permissions(ST_PATTY_CHANNEL.guild.default_role, view_channel=False)
+            if EASTER_CHANNEL:
+                if (get_easter_date(date.today().year) - timedelta(days=3) <= date.today() <= get_easter_date(date.today().year) + timedelta(days=1)):
+                    await EASTER_CHANNEL.set_permissions(EASTER_CHANNEL.guild.default_role, view_channel=None)
+                else:
+                    await EASTER_CHANNEL.set_permissions(EASTER_CHANNEL.guild.default_role, view_channel=False)
 
-            if (get_easter_date(date.today().year) - timedelta(days=3) <= date.today() <= get_easter_date(date.today().year) + timedelta(days=1)):
-                await EASTER_CHANNEL.set_permissions(EASTER_CHANNEL.guild.default_role, view_channel=None)
-            else:
-                await EASTER_CHANNEL.set_permissions(EASTER_CHANNEL.guild.default_role, view_channel=False)
+            if CDM_CHANNEL:
+                if (date(date.today().year, 5, 5) - timedelta(days=1) <= date.today() <= date(date.today().year, 5, 5) + timedelta(days=1)):
+                    await CDM_CHANNEL.set_permissions(CDM_CHANNEL.guild.default_role, view_channel=None)
+                else:
+                    await CDM_CHANNEL.set_permissions(CDM_CHANNEL.guild.default_role, view_channel=False)
 
-            if (date(date.today().year, 5, 5) - timedelta(days=1) <= date.today() <= date(date.today().year, 5, 5) + timedelta(days=1)):
-                await CDM_CHANNEL.set_permissions(CDM_CHANNEL.guild.default_role, view_channel=None)
-            else:
-                await CDM_CHANNEL.set_permissions(CDM_CHANNEL.guild.default_role, view_channel=False)
+            if J4_CHANNEL:
+                if (date(date.today().year, 7, 4) - timedelta(days=1) <= date.today() <= date(date.today().year, 7, 4) + timedelta(days=1)):
+                    await J4_CHANNEL.set_permissions(J4_CHANNEL.guild.default_role, view_channel=None)
+                else:
+                    await J4_CHANNEL.set_permissions(J4_CHANNEL.guild.default_role, view_channel=False)
 
-            if (date(date.today().year, 7, 4) - timedelta(days=1) <= date.today() <= date(date.today().year, 7, 4) + timedelta(days=1)):
-                await J4_CHANNEL.set_permissions(J4_CHANNEL.guild.default_role, view_channel=None)
-            else:
-                await J4_CHANNEL.set_permissions(J4_CHANNEL.guild.default_role, view_channel=False)
+            if HALLOWEEN_CHANNEL:
+                if (date(date.today().year, 10, 1) - timedelta(days=1) <= date.today() <= date(date.today().year, 10, 31) + timedelta(days=1)):
+                    await HALLOWEEN_CHANNEL.set_permissions(HALLOWEEN_CHANNEL.guild.default_role, view_channel=None)
+                else:
+                    await HALLOWEEN_CHANNEL.set_permissions(HALLOWEEN_CHANNEL.guild.default_role, view_channel=False)
 
-            if (date(date.today().year, 10, 1) - timedelta(days=1) <= date.today() <= date(date.today().year, 10, 31) + timedelta(days=1)):
-                await HALLOWEEN_CHANNEL.set_permissions(HALLOWEEN_CHANNEL.guild.default_role, view_channel=None)
-            else:
-                await HALLOWEEN_CHANNEL.set_permissions(HALLOWEEN_CHANNEL.guild.default_role, view_channel=False)
+            if THANKSGIVING_CHANNEL:
+                if (date(date.today().year, 11, 1) - timedelta(days=1) <= date.today() <= get_thanksgiving_date(date.today().year) + timedelta(days=1)):
+                    await THANKSGIVING_CHANNEL.set_permissions(THANKSGIVING_CHANNEL.guild.default_role, view_channel=None)
+                else:
+                    await THANKSGIVING_CHANNEL.set_permissions(THANKSGIVING_CHANNEL.guild.default_role, view_channel=False)
 
-            if (date(date.today().year, 11, 1) - timedelta(days=1) <= date.today() <= get_thanksgiving_date(date.today().year) + timedelta(days=1)):
-                await THANKSGIVING_CHANNEL.set_permissions(THANKSGIVING_CHANNEL.guild.default_role, view_channel=None)
-            else:
-                await THANKSGIVING_CHANNEL.set_permissions(THANKSGIVING_CHANNEL.guild.default_role, view_channel=False)
-
-            if (get_thanksgiving_date(date.today().year) - timedelta(days=1) <= date.today() <= date(date.today().year, 12, 25) + timedelta(days=1)):
-                await CHRISTMAS_CHANNEL.set_permissions(CHRISTMAS_CHANNEL.guild.default_role, view_channel=None)
-                await WISHLIST_CHANNEL.set_permissions(WISHLIST_CHANNEL.guild.default_role, view_channel=None)
-            else:
-                await CHRISTMAS_CHANNEL.set_permissions(CHRISTMAS_CHANNEL.guild.default_role, view_channel=False)
-                await WISHLIST_CHANNEL.set_permissions(WISHLIST_CHANNEL.guild.default_role, view_channel=False)
+            if CHRISTMAS_CHANNEL and WISHLIST_CHANNEL:
+                if (get_thanksgiving_date(date.today().year) - timedelta(days=1) <= date.today() <= date(date.today().year, 12, 25) + timedelta(days=1)):
+                    await CHRISTMAS_CHANNEL.set_permissions(CHRISTMAS_CHANNEL.guild.default_role, view_channel=None)
+                    await WISHLIST_CHANNEL.set_permissions(WISHLIST_CHANNEL.guild.default_role, view_channel=None)
+                else:
+                    await CHRISTMAS_CHANNEL.set_permissions(CHRISTMAS_CHANNEL.guild.default_role, view_channel=False)
+                    await WISHLIST_CHANNEL.set_permissions(WISHLIST_CHANNEL.guild.default_role, view_channel=False)
 
 async def setup(bot):
         await bot.add_cog(Holidays(bot))
