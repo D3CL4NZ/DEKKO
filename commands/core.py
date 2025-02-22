6ef7723d1f61c917920eab0a14b90c442dbd68a8
Declan Zevan <declan.zevan@gmail.com>
2025-02-21 21:37:56 -0500

    I'm not sure why this is necessary

--- commands/core.py
+++ commands/core.py
@@ -53,20 +53,21 @@ Started: <t:{}:R>""".format(ctx.command, ctx.author.mention, int(time.time())))
     async def _ping(self, ctx):
         """Checks DEKKO's connection to the server"""
 
-        start = time.perf_counter()
-        message = await ctx.send("Resolving...")
-        end = time.perf_counter()
-        
-        duration = (end - start) * 1000
+        async with ctx.typing():
+            start = time.perf_counter()
+            message = await ctx.send("Resolving...")
+            end = time.perf_counter()
+            
+            duration = (end - start) * 1000
 
-        embed = discord.Embed(
-            title=None,
-            description=":ping_pong: **Pong!**",
-            color=discord.Colour.blurple()
-        )
-        embed.set_footer(text=f"Web socket latency: {round(self.bot.latency * 1000)}ms | Total latency: {duration:.0f}ms")
+            embed = discord.Embed(
+                title=None,
+                description=":ping_pong: **Pong!**",
+                color=discord.Colour.blurple()
+            )
+            embed.set_footer(text=f"Web socket latency: {round(self.bot.latency * 1000)}ms | Total latency: {duration:.0f}ms")
 
-        message = await message.edit(content=None, embed=embed)
+            message = await message.edit(content=None, embed=embed)
 
     @commands.hybrid_command(name='shutdown', with_app_command=True)
     @app_commands.allowed_installs(guilds=True, users=False)
@@ -75,10 +76,11 @@ Started: <t:{}:R>""".format(ctx.command, ctx.author.mention, int(time.time())))
     async def _shutdown(self, ctx):
         """Shuts down the bot"""
 
-        message = await ctx.send(":hourglass:  **DEKKO is shutting down...**")
+        async with ctx.typing():
+            message = await ctx.send(":hourglass:  **DEKKO is shutting down...**")
 
-        await message.edit(content=":electric_plug: **DEKKO is now offline**")
-        await self.bot.close()
+            await message.edit(content=":electric_plug: **DEKKO is now offline**")
+            await self.bot.close()
 
     @commands.hybrid_command(name='reboot', with_app_command=True)
     @app_commands.allowed_installs(guilds=True, users=False)
@@ -86,29 +88,30 @@ Started: <t:{}:R>""".format(ctx.command, ctx.author.mention, int(time.time())))
     @commands.is_owner()
     async def _reboot(self, ctx):
         """Reboots DEKKO"""
-        await self.bot.change_presence(status=discord.Status.dnd, activity=discord.CustomActivity(name=f"DEKKO is rebooting..."))
+        async with ctx.typing():
+            await self.bot.change_presence(status=discord.Status.dnd, activity=discord.CustomActivity(name=f"DEKKO is rebooting..."))
 
-        log_channel_id = await db.fetch_one("SELECT global_log_channel FROM global_config")
-        log_channel = self.bot.get_channel(log_channel_id[0]) if log_channel_id else None
+            log_channel_id = await db.fetch_one("SELECT global_log_channel FROM global_config")
+            log_channel = self.bot.get_channel(log_channel_id[0]) if log_channel_id else None
 
-        message = await ctx.send(f":hourglass:  **Reboot <t:{int(time.time()) + 5}:R>**")
-        time.sleep(4)
+            message = await ctx.send(f":hourglass:  **Reboot <t:{int(time.time()) + 5}:R>**")
+            time.sleep(4)
 
-        if log_channel:
-            embed = discord.Embed(
-                title=None,
-                description=f":electric_plug: **DEKKO was rebooted**",
-                color=discord.Colour.red()
-            )
-            embed.timestamp = discord.utils.utcnow()
+            if log_channel:
+                embed = discord.Embed(
+                    title=None,
+                    description=f":electric_plug: **DEKKO was rebooted**",
+                    color=discord.Colour.red()
+                )
+                embed.timestamp = discord.utils.utcnow()
 
-            await log_channel.send(embed=embed)
+                await log_channel.send(embed=embed)
 
-        await message.edit(content=":electric_plug: **Rebooting...**")
-        time.sleep(5)
-        await self.bot.close()
-        time.sleep(5)
-        os.execl(sys.executable, sys.executable, *sys.argv)
+            await message.edit(content=":electric_plug: **Rebooting...**")
+            time.sleep(5)
+            await self.bot.close()
+            time.sleep(5)
+            os.execl(sys.executable, sys.executable, *sys.argv)
 
 async def setup(bot):
     await bot.add_cog(Core(bot))
