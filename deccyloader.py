9916f2f9f9dc476da8e51b4e9dea824f962f6b36
Declan Zevan <58536897+D3CL4NZ@users.noreply.github.com>
2025-10-20 23:30:39 -0400

    Change loaded extensions message emoji

--- deccyloader.py
+++ deccyloader.py
@@ -80,7 +80,7 @@ class DeccyLoader(commands.Cog):
     @commands.is_owner()
     async def _list(self, ctx):
         """Lists all loaded extensions"""
-        await ctx.send(f':white_check_mark:  **Loaded extensions:**\n```{", ".join(self.bot.extensions)}```')
+        await ctx.send(f':jigsaw:  **Loaded extensions:**\n```{", ".join(self.bot.extensions)}```')
 
 async def setup(bot):
     await bot.add_cog(DeccyLoader(bot))
